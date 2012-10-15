92243edaa908427c192dd110e4c82ca42f8be5b9
Marco Túlio Ribeiro <marcotcr@zunnit.com>
2012-10-15 17:09:51 -0300

    Simplified generate_input.py

--- baseline/generate_input.py
+++ baseline/generate_input.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python
-"""Given an dataset in movielens1M/10M format and a X, generates a training and a test file
+"""Given an dataset in movielens1M/10M format and a percentage X, generates a training and a test file
   for using on MyMediaLite Algorithms, with X percent of the size from the input being used as training
-Author: Arthur Barbosa C^amara (camara.arthur@{gmail,dcc.ufmg}.com) 
+Authors: Arthur Barbosa Camara (camara.arthur@{gmail,dcc.ufmg}.com) 
+         Marco Tulio Correia Ribeiro (marcotcr@gmail.com)
 """
+
+# Makes float division the default (instead of integer division)
+from __future__ import division
 
 import sys
 import getopt
 import math
 import random
+import os
 
 def Usage():
   print "%s" % sys.argv[0]
   print "Options: "
   print "-i (required) ratings file - using MovieLens1M format"
   print "-o (required) output file prefix. Final output is option_train and option_test"
-  print "-p (optional) Percentage of the file to be used as training. Default as 0.8"
+  print "-p (optional) Percentage of the file to be used as training."
+  print 'Default is 0.8. This must be a number between 0 and 1'
   print "Example Usage:"
   print "./generate_input.py -i /mnt/hd0/marcotcr/datasets/movielens1M/ratings.dat -o out -p .80"
   sys.exit(2)
-def File_Size(fname):
+def FileSize(fname):
   """
-  Count how many inputs there is in a file
+  Counts how many lines there are in a file.
+  Returns an integer.
   """
-  with open(fname) as f:
-    for i, l in enumerate(f):
-      pass
-  return i+1
+  return int(os.popen('wc -l %s' % fname).read().split()[0])
 
 def main():
   try: 
@@ -42,37 +46,35 @@ def main():
     if option == "-i":
       in_file = value
     elif option == "-o":
-      train_file = value+"_train"
-      test_file  = value+"_test"
+      train_file = value + '_train'
+      test_file  = value + '_test'
     elif option == "-p":
       percentage = float(value)
     else:
       assert False, "Option %s is not avaiable" % option
-  if not  in_file or not train_file or not test_file:
+  if not in_file or not train_file or not test_file:
     print "Something is missing"
     Usage()
 
   #Get the input size
-  in_size = File_Size(in_file)
-  test_size = in_size*percentage
+  in_size = FileSize(in_file)
+  test_size = in_size * (1 - percentage)
 
   train_file = open(train_file, "w")
   test_file  = open(test_file,  "w")
 
+  # Reads the data from the infile
+  data = open(in_file, 'r').readlines()
+
   #Shuffle lines of a file
-  with open(in_file, 'r') as source:
-    data = [ (random.random(), line) for line in source ]
-    data.sort()
-  
-  i=0
+  random.shuffle(data)
   
   #write the shuffled lines on the output files
-  for _,line in data:
+  for i , line in enumerate(data):
     if i <= test_size:
       test_file.write(line)
     elif i > test_size:
       train_file.write(line)
-    i = i+1
 
 if __name__ == "__main__":
   main()
