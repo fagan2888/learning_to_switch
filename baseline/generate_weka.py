a15759476d2800cf130752821ac4abd0a6b0997e
Arthur Câmara <camara.arthur@gmail.com>
2012-10-16 18:23:23 -0300

    Geração de dicionário indexado por tuplas.

--- baseline/generate_weka.py
+++ baseline/generate_weka.py
@@ -13,9 +13,12 @@ Author: Arthur Barbosa C^amara (camara.arthur@{gmail.com, dcc.ufmg.br})
 def Usage():
   print "%s" %sys.argv[0]
   print "Options:"
-  print "-a (optional) List of algorithms to be executed as level1-predictors. Default all"
-  print "-p (optional) Percentage of the split from the dataset to be used as training. Default 0.80"
-  print "-i (required) input dataset file"
+  print "-a (optional) List of algorithms to be used as level1-predictors. Default all"
+  print "-i (required) input file format. Wil be used as <algorithm>_<input_format>"
+  print "-u (optional) user attributes file"
+  print "-t (optional) item attributes file"
+  print "-f (required) training file"
+  print "-e (required) test file"
   print "-o (required) output file" 
   print "Example Usage:"
   print "./runtime_metrics.py -i /mnt/hd0/marcotcr/datasets/movielens1m/ratings.dat -o out.txt"
@@ -26,7 +29,7 @@ global u = dict()
 
 
 def  GenerateRM1(filename):
-""" Generates a dictionary with the first RunTimeMetric, itens rated by a given user.
+""" Generates a dictionary with the first RunTimeMetric, giving how many itens has been rated by a given user. map[user]->#
 """
   i = dict()
   in_file = open(filename, "r")
@@ -42,7 +45,7 @@ def  GenerateRM1(filename):
     
     
 def GenerateRM2(filename):
-"""Generates a dictionary with the second RunTimemetric, how many times a given item has been rated
+"""Generates a dictionary with the second RunTimeMetric, giving how many times a given item has been rated map[item]->#
 """
   u = dict()
   in_file = open(filename, "r")
@@ -65,21 +68,39 @@ def ListToString(l):
 
   return l
 
+def  GenerateRatings(file_format, algorithms):
+  """Generates a dictionary, with tuples as keys that maps[(user_id,movie_id, algorithm)]->rating
+    Receives an algorithm list and the file format used for the inputs
+  """
+  ratings = {}
+  for algorithm in algorithms:
+    in_file = open(algorithm+ '_' + file_format, 'r')
+    for line in in_file:
+      linep = line.split(" ")
+      ratings[(int(linep[0]), int(linep[1]), algorithm)] = float(linep[2])
+
+def PrintWeka(out_file, algorithms, )
+
+
+
+
 def main():
   try: 
-    opts, args = getopt.getopt(sys.argv[1:], "i:o:a:p:f:u:m:")
+    opts, args = getopt.getopt(sys.argv[1:], "i:a:u:t:f:e:o:")
   except getopt.GetoptError, err:
     print str(err)
     Usage()
   
   itens_rated_by_user = dict()
   users_rated_item    = dict()
+  ratings             = {}
   percentage          = '0.80'
+  in_file_format      = None
   in_file             = None
   out_file            = None
   out_folder          = None
-  test_file           = outlvl1_test
-  train_file          = outlvl1_train
+  test_file           = None 
+  train_file          = None
   user_attributes     = None
   item_attributes     = None
 
@@ -91,42 +112,36 @@ def main():
   'LatentFeatureLogLinearModel', 'BiasedMatrixFactorization', 'SVDPlusPlus',
   'SigmoidSVDPlusPlus', 'SigmoidItemAsymmetricFactorModel',
   'SigmoidUserAsymmetricFactorModel', 'SigmoidCombinedAsymmetricFactorModel']
-  
+
   for option, value in opts:
     if option == "-i":
-      in_file = value
-    elif option == "-o":
-      out_file = value
+      in_file_format = value
     elif option == "-a":
       algorithms = value.split(",")
-    elif option == "-p":
-      percentage = value
-    elif option == "-f":
-      out_folder = value
     elif option == "-u":
       user_attributes = value
-    elif options == "-m":
+    elif option =="-t":
       item_attributes = value
+    elif option == "-f":
+      train_file = value
+    elif option == "-e":
+      test_file = value
+    elif option == "-o":
+      out_file = value
     else:
       assert False, "option %s is not avaiable" % option
   if not in_file or not out_file:
     Usage()
-  
-  #Split dataset for test and train sets
-  cmd = "./generate_input.py -i %s -o outlvl1 -p %s" % (in_file, percentage)
-  os.system(cmd)
-
-  #With the dataset splitted as train and test, we can run level 1 predictors and runtime metrics
-  #Run level1-predictor engines
-    cmd = "./level1_predictors -a %s -t outlvl1 -o %s -i %s -u %s" % (ListToString(algorithms), out_folder, item_attributes, user_attributes)
 
   #Defines the Runtime metrics to be used, as dictionaries
-  itens_rated_by_user = GenerateRM1(in_file)
-  users_rated_item    = GenerateRM2(in_file)
+  itens_rated_by_user = GenerateRM1(train_file)
+  users_rated_item    = GenerateRM2(train_file)
+
+  #generates a tuple-indexed dictionary ratings[(user_id, movie_id, algorithm')] -> rating
+  ratings = GenerateRatings(in_file_format, algorithms)
+
+  #generates Weka formatted output file
   
-  #Next step is to generate Weka input file. First, generate each line, with the ratings from every algorithm and the runtimemetrics
-
-
 
 if __name__ == "__main__":
   main()
