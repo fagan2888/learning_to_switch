9222f3aded5027d6ca7b43c5d3e4ad2e50f8aebb
Arthur <camara.arthur@gmail.com>
2012-11-27 00:55:15 -0200

    Funcionando, mas não minimiza o suficiente

--- logistic_regression.py
+++ logistic_regression.py
@@ -16,7 +16,7 @@ import re
 
 from numpy import *
 from numpy import e as npe
-from scipy.optimize import fmin_bfgs
+from scipy.optimize import fmin
 
 def Usage():
   print "%s" %sys.argv[0]
@@ -27,8 +27,19 @@ def Usage():
   print "-s (optional) Data separator for test and train files. default as space"
   sys.exit(2)
 
+
 def Sigmoid(x):
-  return 1/(1+power(npe,((-1)*x)))
+  h = 1.0/(1.0+ npe**((-1.0)*x))
+  return h
+
+
+def PreProcessing(X,Y,cl,delta):
+  for i in range (size(Y)):
+    if abs(X[cl][i]-Y[i]) <= delta:
+      Y[i] = 1
+    else:
+      Y[i] = 0
+  return (X, Y)
 
 
 def IsClass(Y, n):
@@ -42,35 +53,28 @@ def IsClass(Y, n):
   return Y
 
 def Cost2(theta, X, Y):
-  m,n = X.shape
-  h = Sigmoid(X.dot(theta))
+  m = X.shape[0]
+  h = Sigmoid(X.dot(theta.T))
   thetaR = theta
-  return (1.0/m) * ((-Y.T.dot(log(h))) - ((1 - Y.T).dot(log(1.0-h)))) \
-        + (1/(2.0*m)) * (thetaR.T.dot(thetaR))
-
-def Cost(theta, X, Y):
-  print "COST"
-  print "******************************************"
-  h = Sigmoid(X.dot(theta))
-  thetaR = theta[1:, 0]
-  m, n = X.shape
-
-  J = (1.0 /m) * ((-Y.T.dot(log(h))) - ((1-Y.T).dot(log(1.0-h)))) \
-       + (1/(2.0 *m)) * (thetaR.T.dot(thetaR))
+  J = ((1.0/m) * ((-Y.T.dot(log(h))) - ((1 - Y.T).dot(log(1.0-h))))).flatten()
+  print "COST = "
   print J
-  delta = h-Y
-  sumdelta = delta.T.dot(X[:,1])
-  grad1 = (1.0/m) * sumdelta
-  XR = X[: 1:X.shape[1]]
-  sumdelta = delta.T.dot(XR)
-  
-  grad = (1.0/m) * (sumdelta + thetaR)
-  
-  out = zeros(shape=(grad.shape[0], grad.shape[1] + 1))
-  out[:, 0] = grad1
-  out[:, 1:] = grad
+  return J
 
-  return J.flatten(), out.T.flatten()
+def predict(theta, X):
+  m, n = X.shape
+  p = zeros(shape=(m,1))
+
+  h = Sigmoid(X.dot(theta.T))
+
+  print h
+  for i in range (0, h.shape[0]):
+    if h[i] >= 0.5:
+      h[i] = 1
+    else:
+      h[i] = 0
+
+  return h 
 
 
 def decorated_cost(theta, y, X):
@@ -83,6 +87,7 @@ def main():
   X           = None
   Y           = []
   theta       = []
+  result      = []
   i           = 0
   
   try:
@@ -121,25 +126,42 @@ def main():
   m,n = X.shape
   Y.shape=(m,1)
   initial_theta = zeros(shape = (X.shape[1],1))
-    
-  #def decorated_cost(theta, y):
-   # return Cost2(theta, X, y)
+  
+  print Y
+
+  #Simple test:
+  theta = fmin(decorated_cost, initial_theta, args = (Y,X))
+  print theta
+  print predict(theta, X)
 
   #train for each possible class. When testing, will select the one that fits better.
-  for i in range(0, n):
-    y = IsClass(Y, i)
-    theta.append(fmin_bfgs(decorated_cost, initial_theta, args=(y, X)))
-  for i in range(0,n):
-    print theta[i]
+  #for i in range(0, n):
+    #print "TRAINNED"
+    #y = IsClass(Y, i) 
+    #t1 = (fmin(decorated_cost, initial_theta, args=(y, X)))
+    #print t1
+    #theta.append(t1)
+  #for i in range(0,n):
+   # print theta[i]
   #Test for every entrance on test file -  get the probability for each trainned model
   test_file = open(test_file, "r")
+  i=0
   for line in test_file:
-    for i in range(0, n):
-      linep = line.split(separator)
-      result = int(linep.pop())
-      linep = map(float,linep)
-      
-
+    linep = line.split(separator)
+    result.append(int(linep.pop().strip('\n')))
+    linep = map(float,linep)
+    if i ==0:
+      Xt = array(linep)
+      i = 1
+    else:
+      Xt = vstack((Xt, array(linep)))
+  result = array(result)
+  m,n = Xt.shape
+  result.shape=(m,1)
+  #prediction time:
+  p = predict(theta, Xt)
+  print p
+  print result
 
 
 if __name__ == '__main__':
